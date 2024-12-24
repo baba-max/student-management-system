b63b977a51de3e10f3ccaa5a822a88b5179ececc
baba-max <mbabayev558@gmail.com>
2024-12-24 12:35:55 +0400

    writing exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,16 +1,55 @@
-from fastapi import HTTPException,status
+from fastapi import HTTPException, status
 
 
 class DetailedHTTPException(HTTPException):
     STATUS_CODE = status.HTTP_500_INTERNAL_SERVER_ERROR
     DETAIL = "server error"
-    
-    def __init__(self):
-        super().__init__(status_code=self.STATUS_CODE,
-                         detail=self.DETAIL)
 
+    def __init__(self):
+        super().__init__(status_code=self.STATUS_CODE, detail=self.DETAIL)
 
 
 class UserNotFoundException(DetailedHTTPException):
-    STATUS_CODE=status.HTTP_404_NOT_FOUND
-    DETAIL="user is not found"        
+    STATUS_CODE = status.HTTP_404_NOT_FOUND
+    DETAIL = "User is not found"
+
+
+class InvalidPassword(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_401_UNAUTHORIZED
+    DETAIL = "Invalid password"
+
+
+class UserAlreadyExist(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_409_CONFLICT
+    DETAIL = "User already exist"
+
+
+class InvalidRole(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_403_FORBIDDEN
+    DETAIL = "The role can only be admin or lecturer."
+
+
+class StudentAlreadyExist(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_409_CONFLICT
+    DETAIL = "Student already exist"
+
+
+class StudentNotFoundException(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_404_NOT_FOUND
+    DETAIL = "Student is not found"
+    
+class CourseAlreadyExist(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_409_CONFLICT
+    DETAIL = "Course already exist"
+    
+class LecturerNotFoundException(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_404_NOT_FOUND
+    DETAIL = "lecturer is not found"
+    
+class SubjectNotFoundException(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_404_NOT_FOUND
+    DETAIL = "Subject is not found"
+    
+class CourseNotFoundException(DetailedHTTPException):
+    STATUS_CODE = status.HTTP_404_NOT_FOUND
+    DETAIL = "Course is not found"
